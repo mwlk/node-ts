2d3418c677db663b57cd17ca94bc5569dc651730
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-09-02 00:11:53 -0300

    add dbcontext

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import {_router} from './routes'
+import { _router } from './routes'
+import db from './config/mongo'
 
 const PORT = process.env.PORT || 3023
 
-const app: express.Application = express();
+const app: express.Application = express()
 app.use(cors())
 app.use(_router)
+
+db()
+  .then(() => {
+    console.log('mongo connect')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 app.listen(PORT, () => {
   console.log(`port ${PORT}`)
