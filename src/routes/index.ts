3a4c555737957ccdfdb88e1305fd4b67acebc6d4
Mirko Wlk <mirkoivowlk@gmail.com>
2022-10-15 18:15:32 -0300

    update routes index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,22 +1,26 @@
-import { Router } from 'express'
-import { readdirSync } from 'fs'
+import { Router } from "express";
+import { readdirSync } from "fs";
 
-const PATH_ROUTER = `${__dirname}`
-const _router = Router()
+const PATH_ROUTER = `${__dirname}`;
+const _router = Router();
 
 const cleanFileName = (fileName: string) => {
-  const file = fileName.split('.').shift()
-  return file
-}
+  const file = fileName.split(".").shift();
+  return file;
+};
 
 readdirSync(PATH_ROUTER).filter((fileName) => {
-  const cleanName = cleanFileName(fileName)
-  if (cleanName !== 'index') {
-    import(`./${cleanName}`).then((routerModule) => {
-      console.log(`loading route ..... ${cleanName}`)
-      _router.use(`/${cleanName}`, routerModule._router)
-    })
+  const cleanName = cleanFileName(fileName);
+  if (cleanName !== "index") {
+    import(`./${cleanName}`)
+      .then((routerModule) => {
+        console.log(`loading route ..... ${cleanName}`);
+        _router.use(`/${cleanName}`, routerModule._router);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
-})
+});
 
-export { _router }
+export { _router };
