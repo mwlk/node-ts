59e5a7c7af3a58375fecdbb594c31a2fe01da2b5
Mirko Wlk <mirkoivowlk@gmail.com>
2022-10-15 18:14:18 -0300

    format

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,24 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import { _router } from './routes'
-import db from './config/mongo'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { _router } from "./routes";
+import db from "./config/mongo";
 
-const PORT = process.env.PORT || 3023
+const PORT = process.env.PORT || 3023;
 
-const app: express.Application = express()
-app.use(cors())
-app.use(_router)
+const app: express.Application = express();
+app.use(cors());
+app.use(express.json());
+app.use(_router);
 
 db()
   .then(() => {
-    console.log('mongo connect')
+    console.log("mongo connect");
   })
   .catch((err) => {
-    console.log(err)
-  })
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
-  console.log(`port ${PORT}`)
-})
+  console.log(`port ${PORT}`);
+});
