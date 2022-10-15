6e3d96602e0ae9293f2603f4c7f8d8a59b7e4179
Mirko Wlk <mirkoivowlk@gmail.com>
2022-10-15 18:15:08 -0300

    update item controller

--- src/controllers/item.ts
+++ src/controllers/item.ts
@@ -1,32 +1,39 @@
-import { Request, Response } from 'express'
-import { handleHttp } from '../utils/error.handler'
+import { Request, Response } from "express";
+import { handleHttp } from "../utils/error.handler";
 
 const getItem = (req: Request, res: Response) => {
   try {
   } catch (e) {
-    handleHttp(res, 'ERROR_GET_ITEM')
+    handleHttp(res, "ERROR_GET_ITEM");
   }
-}
+};
 
 const getItems = (req: Request, res: Response) => {
   try {
   } catch (e) {
-    handleHttp(res, 'ERROR_GET_ALL_ITEMS')
+    handleHttp(res, "ERROR_GET_ALL_ITEMS");
   }
-}
+};
 
 const updateItem = (req: Request, res: Response) => {
   try {
   } catch (e) {
-    handleHttp(res, 'ERROR_UPD_ITEM')
+    handleHttp(res, "ERROR_UPD_ITEM");
   }
-}
+};
 
 const addItem = (req: Request, res: Response) => {
   try {
+    console.log(req.body);
   } catch (e) {
-    handleHttp(res, 'ERROR_POST_ITEM')
+    handleHttp(res, "ERROR_POST_ITEM");
   }
-}
+};
+const deleteItem = (req: Request, res: Response) => {
+  try {
+  } catch (e) {
+    handleHttp(res, "ERROR_DELETE_ITEM");
+  }
+};
 
-export { getItem, getItems, updateItem, addItem }
+export { getItem, getItems, updateItem, addItem, deleteItem };
