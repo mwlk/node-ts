2cc98c2cae6fb534f93a07be960ecd4ca3d6cc17
Mirko Wlk <mirkoivowlk@gmail.com>
2022-10-15 21:05:11 -0300

    complete items controller

--- src/controllers/item.ts
+++ src/controllers/item.ts
@@ -1,38 +1,60 @@
 import { Request, Response } from "express";
+import {
+  deleteCar,
+  getCarDetail,
+  getCars,
+  insertCar,
+  updateCar,
+} from "../services/item";
 import { handleHttp } from "../utils/error.handler";
 
-const getItem = (req: Request, res: Response) => {
+const getItem = async (req: Request, res: Response) => {
   try {
+    const id = req.params.id;
+    const response = await getCarDetail(id);
+    const data = response ? response : `Not Found Car ${id}`;
+
+    res.send(data);
   } catch (e) {
     handleHttp(res, "ERROR_GET_ITEM");
   }
 };
 
-const getItems = (req: Request, res: Response) => {
+const getItems = async (req: Request, res: Response) => {
   try {
+    const response = await getCars();
+    res.send(response);
   } catch (e) {
     handleHttp(res, "ERROR_GET_ALL_ITEMS");
   }
 };
 
-const updateItem = (req: Request, res: Response) => {
+const updateItem = async ({ params, body }: Request, res: Response) => {
   try {
+    const { id } = params;
+    const response = await updateCar(id, body);
+    res.send(response);
   } catch (e) {
     handleHttp(res, "ERROR_UPD_ITEM");
   }
 };
 
-const addItem = (req: Request, res: Response) => {
+const addItem = async (req: Request, res: Response) => {
   try {
-    console.log(req.body);
+    // console.log(req.body);
+    const response = await insertCar(req.body);
+    res.send(response);
   } catch (e) {
-    handleHttp(res, "ERROR_POST_ITEM");
+    handleHttp(res, "ERROR_POST_ITEM", e);
   }
 };
-const deleteItem = (req: Request, res: Response) => {
+const deleteItem = async ({ params }: Request, res: Response) => {
   try {
+    const { id } = params;
+    const response = await deleteCar(id);
+    res.send(response);
   } catch (e) {
-    handleHttp(res, "ERROR_DELETE_ITEM");
+    handleHttp(res, "ERROR_DELETE_ITEM", e);
   }
 };
 
